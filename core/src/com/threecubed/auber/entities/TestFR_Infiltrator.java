f004f0e1b1864ac34a667470c89c9dc20473ed7d
MirceaZisu <62066453+MirceaZisu@users.noreply.github.com>
2021-02-04 18:42:21 +0000

    Infitltrator Test and attempt at libgdx test

---
+++ core/src/com/threecubed/auber/entities/TestFR_Infiltrator.java
@@ -0,0 +1,23 @@
+package com.threecubed.auber.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import com.threecubed.auber.World;
+
+
+//Headless.loadHeadless();
+
+class TestFR_InfiltratorCount {
+	  /**
+	   * Test whether there are 8 infiltrators in the game
+	   *
+	   * @author Mircea Zisu
+	   * @version 1.0
+	   * */
+	@Test
+	void testInfiltratorCount() {
+		assertEquals(8, World.MAX_INFILTRATORS);
+	}
+
+}
